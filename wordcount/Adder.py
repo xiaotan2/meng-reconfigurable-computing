65a4c61e0568d8414e148593f55f99488763dd20
hm492 <hm492@cornell.edu>
2015-11-16 19:20:44 -0500

    Adder wrapper and test

---
+++ wordcount/Adder.py
@@ -0,0 +1,27 @@
+#=========================================================================
+# Adder Wrapper
+#=========================================================================
+
+
+from pymtl  import *
+
+class Adder ( VerilogModel ):
+  
+  vprefix    = ""
+  vlinetrace = True
+
+  def __int__( s ):
+   
+  s.in0 = InPort  ( Bits(16) )
+  s.in1 = InPort  ( Bits(16) ) 
+  s.out = OutPort ( Bits(16) )
+
+  s.set_ports({
+    'in0'   :  s.in0,
+    'in1'   :  s.in1,
+    'out'   :  s.out, 
+  })
+
+  def line_trace( s ):
+    return "{} () {} () {}".format( s.in0, s.in1, s.out)
+
