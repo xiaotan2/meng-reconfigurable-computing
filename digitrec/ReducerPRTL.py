cf4c20fafa91a04cf244d39f932fd12a82501100
mq58 <mq58@cornell.edu>
2015-11-30 00:41:14 -0500

    finished initial state and tested

--- digitrec/ReducerPRTL.py
+++ digitrec/ReducerPRTL.py
@@ -2,15 +2,18 @@
 # Reducer
 #=========================================================================
 
+import math
+
 from pymtl         import *
 from pclib.ifcs    import InValRdyBundle, OutValRdyBundle
-from pclib.rtl     import RegEn, Mux, Adder, ZeroExtender, RegRst
+from pclib.rtl     import RegEnRst, Mux, Adder, ZeroExtender, RegRst
 from ReducerMsg    import ReducerReqMsg, ReducerRespMsg
 from pclib.ifcs    import MemReqMsg, MemRespMsg
 
 DIGIT = 10
 TYPE_READ = 0
 TYPE_WRITE = 1
+MEM_DATA_SIZE = 56
 
 #=========================================================================
 # Reducer Datapath
@@ -25,57 +28,60 @@ class ReducerDpath (Model):
     s.resp_msg_data     = OutPort  (49)
     s.resp_msg_digit    = OutPort  ( 4)
   
-    s.mem_req_msg_data   = OutPort (49)
+    s.mem_req_msg_data   = OutPort (MEM_DATA_SIZE)
  
-    s.mem_resp_msg_data  = InPort  (49)
+    s.mem_resp_msg_data  = InPort  (MEM_DATA_SIZE)
    
     # ctrl->dpath
     s.knn_reg_en     = InPort (1)
     s.max_reg_en     = InPort (1)
-    s.max_sel_en     = InPort (1)
+    s.max_mux_sel    = InPort (1)
     s.sum_reg_en     = InPort (1)
 
     # dpath->ctrl
     s.update_knn     = OutPort(1)
 
-    # Input Mux    
-    s.reg_out = Wire(32)
+    # initialize knn_table in memory
+    s.mem_req_msg_data = 50
 
-    s.mux = m = Mux( 32, 2)
-    s.connect_dict({
-      m.sel     : s.sel,
-      m.in_[0]  : 0,
-      m.in_[1]  : s.reg_out
-    })
-    
-
-    # Output Register
-    s.adder_out = Wire(32)    
-
-    s.reg = m = RegEn( 32 )
-    s.connect_dict({
-      m.en      : s.en,
-      m.in_     : s.adder_out,
-      m.out     : s.reg_out
-    })
-
-    # Zero Extender   
-    s.zext = m = ZeroExtender( 1, 32 )
-    s.connect_dict({
-      m.in_     : s.req_msg_data
-    })
-
-    # Adder    
-    s.add = m = Adder( 32 )
-    s.connect_dict({
-      m.in0     : s.zext.out,
-      m.in1     : s.mux.out,
-      m.cin     : 0,
-      m.out     : s.adder_out
-    })
-
-    # Connect to output port
-    s.connect( s.reg_out, s.resp_msg_data )
+#    # Input Mux    
+#    s.reg_out = Wire(32)
+#
+#    s.mux = m = Mux( 32, 2)
+#    s.connect_dict({
+#      m.sel     : s.sel,
+#      m.in_[0]  : 0,
+#      m.in_[1]  : s.reg_out
+#    })
+#    
+#
+#    # Output Register
+#    s.adder_out = Wire(32)    
+#
+#    s.reg = m = RegEn( 32 )
+#    s.connect_dict({
+#      m.en      : s.en,
+#      m.in_     : s.adder_out,
+#      m.out     : s.reg_out
+#    })
+#
+#    # Zero Extender   
+#    s.zext = m = ZeroExtender( 1, 32 )
+#    s.connect_dict({
+#      m.in_     : s.req_msg_data
+#    })
+#
+#    # Adder    
+#    s.add = m = Adder( 32 )
+#    s.connect_dict({
+#      m.in0     : s.zext.out,
+#      m.in1     : s.mux.out,
+#      m.cin     : 0,
+#      m.out     : s.adder_out
+#    })
+#
+#    # Connect to output port
+#    s.connect( s.reg_out, s.resp_msg_data )
     
 
 
@@ -90,35 +96,35 @@ class ReducerCtrl (Model):
   def __init__( s, k = 3 ):
  
     # ctrl to toplevel
-    s.req_val       = InPort  (1)
-    s.req_rdy       = OutPort (1)
+    s.req_val           = InPort  (1)
+    s.req_rdy           = OutPort (1)
 
-    s.resp_val      = OutPort (1)
-    s.resp_rdy      = InPort  (1)
+    s.resp_val          = OutPort (1)
+    s.resp_rdy          = InPort  (1)
 
-    s.req_msg_type  = InPort  (1)    
-    s.resp_msg_type = InPort  (1)    
+    s.req_msg_type      = InPort  (1)    
+    s.resp_msg_type     = OutPort (1)    
 
-    s.mem_req_val   = OutPort (1)
-    s.mem_req_rdy   = InPort  (1)
-    s.mem_req_msg_addr = OutPort (32)
-    s.mem_req_type  = OutPort (3)
+    s.mem_req_val       = OutPort (1)
+    s.mem_req_rdy       = InPort  (1)
+    s.mem_req_msg_addr  = OutPort (20)
+    s.mem_req_msg_type  = OutPort (3)
 
-    s.mem_resp_val  = InPort  (1)
-    s.mem_resp_rdy  = OutPort (1)
+    s.mem_resp_val      = InPort  (1)
+    s.mem_resp_rdy      = OutPort (1)
 
     # ctrl->dpath
-    s.knn_reg_en    = OutPort (1)
-    s.max_reg_en    = OutPort (1)
-    s.max_sel_en    = OutPort (1)
-    s.sum_reg_en    = OutPort (1)
+    s.knn_reg_en        = OutPort (1)
+    s.max_reg_en        = OutPort (1)
+    s.max_mux_sel       = OutPort (1)
+    s.sum_reg_en        = OutPort (1)
 
     # dpath->ctrl
-    s.update_knn    = InPort(1)
+    s.update_knn        = InPort(1)
  
 
-    s.msg_type_reg_en = Wire ( Bits(1) )
-
+    s.msg_type_reg_en   = Wire ( Bits(1) )
+    s.init_go           = Wire ( Bits(1) )
 
     # State element
     s.STATE_IDLE = 0
@@ -128,13 +134,13 @@ class ReducerCtrl (Model):
     s.state = RegRst( 2, reset_value = s.STATE_IDLE )
 
     # Counters
-    s.init_count  = Wire ( 5 ) # max 30
+    s.init_count  = Wire ( int( math.ceil( math.log( k*DIGIT, 2) ) ) ) # max 30
 
     @s.tick
     def counter():
-      if ( curr_state == s.STATE_INIT ):
+      if ( s.init_go == 1 ):
         s.init_count.next = s.init_count + 1
-      else
+      else:
         s.init_count.next = 0
 
     # State Transition Logic
@@ -153,12 +159,12 @@ class ReducerCtrl (Model):
 
       # Transition out of INIT state
       if ( curr_state == s.STATE_INIT ):
-        if ( counter == k*DIGIT ):
+        if ( s.init_count == k*DIGIT - 1 ):
            next_state = s.STATE_DONE 
 
       # Transition out of DONE state
       if ( curr_state == s.STATE_DONE ):
-      #  if ( s.resp_val and s.resp_rdy ):
+        if ( s.resp_val and s.resp_rdy ):
            next_state = s.STATE_IDLE
 
       s.state.in_.value = next_state
@@ -171,25 +177,42 @@ class ReducerCtrl (Model):
 
       # IDLE state
       if current_state == s.STATE_IDLE:
-        s.req_rdy.value  = 1
-        s.resp_val.value = 0
+        s.req_rdy.value          = 1
+        s.resp_val.value         = 0
+       
         s.msg_type_reg_en.value  = 1
+       
+        s.init_go.value          = 0
 
+        s.mem_req_val.value      = 0
+        s.mem_resp_rdy.value     = 0 
+      
       # INI state
       elif current_state == s.STATE_INIT:
         s.req_rdy.value          = 0
         s.resp_val.value         = 0
+       
         s.msg_type_reg_en.value  = 0
+       
+        s.init_go.value          = 1
+       
         s.mem_req_val.value      = 1
-        s.mem_req_msg_addr.value = init_counter * 3
+        s.mem_req_msg_addr.value = s.init_count * 7
         s.mem_req_msg_type.value = TYPE_WRITE
+       
         s.mem_resp_rdy.value     = 1 
  
       # DONE state
       elif current_state == s.STATE_DONE:
-        s.req_rdy.value  = 0
-        s.resp_val.value = 1
+        s.req_rdy.value          = 0
+        s.resp_val.value         = 1
+      
         s.msg_type_reg_en.value  = 0
+      
+        s.init_go.value          = 0
+        
+        s.mem_req_val.value      = 0
+        s.mem_resp_rdy.value     = 1 
 
     
     # Register for resp msg type     
@@ -213,8 +236,8 @@ class ReducerPRTL (Model):
     s.resp    = OutValRdyBundle ( ReducerRespMsg() )
 
     # local memory interface
-    s.mem_req  = InValRdyBundle ( MemReqMsg(  8, 32, 49 ) )
-    s.mem_resp = OutValRdyBundle( MemRespMsg( 8, 49 )     )
+    s.mem_req  = InValRdyBundle ( MemReqMsg(  8, 20, MEM_DATA_SIZE ) )
+    s.mem_resp = OutValRdyBundle( MemRespMsg( 8, MEM_DATA_SIZE )     )
  
     # instantiate dpath and ctrl unit
     s.dpath   = ReducerDpath()
@@ -252,10 +275,10 @@ class ReducerPRTL (Model):
 
     state_str = "? "
     if s.ctrl.state.out == s.ctrl.STATE_IDLE:
-      state_str = "I   "
+      state_str = "IDLE"
     if s.ctrl.state.out == s.ctrl.STATE_INIT:
       state_str = "INIT"
     if s.ctrl.state.out == s.ctrl.STATE_DONE:
-      state_str = "D   "
+      state_str = "DONE"
 
-    return "{} ({} {} {}) {}".format( s.req, s.dpath.adder_out, s.dpath.reg_out, state_str, s.resp )
+    return "{} (count{} addr{} memreqval{} memrespval{} memresp{} {}) {}".format( s.req, s.ctrl.init_count, s.ctrl.mem_req_msg_addr, s.mem_req.val, s.mem_resp.val, s.mem_resp.msg.data, state_str, s.resp )
--- digitrec/ReducerPRTL_test.py
+++ digitrec/ReducerPRTL_test.py
@@ -12,19 +12,25 @@ from pclib.test  import TestSource, TestSink
 from ReducerPRTL import ReducerPRTL
 from ReducerMsg  import ReducerReqMsg, ReducerRespMsg
 
+from pclib.test  import TestMemory
+from pclib.ifcs  import MemMsg, MemReqMsg, MemRespMsg
+
+MEM_DATA_SIZE = 56
+
 #-------------------------------------------------------------------------
 # TestHarness
 #-------------------------------------------------------------------------
 class TestHarness (Model):
 
   def __init__( s, ReducerPRTL, src_msgs, sink_msgs,
-               src_delay, sink_delay,
-               dump_vcd=False, test_verilog=False ):
+                stall_prob, latency, src_delay, sink_delay,
+                dump_vcd=False, test_verilog=False ):
 
     # Instantiate Models
     s.src     = TestSource  ( ReducerReqMsg(),  src_msgs,  src_delay  )
-    s.reducer = ReducerPRTL ()
+    s.reducer = ReducerPRTL
     s.sink    = TestSink    ( ReducerRespMsg(), sink_msgs, sink_delay )
+    s.mem     = TestMemory  ( MemMsg(8,20, MEM_DATA_SIZE), 1, stall_prob, latency )
 
     # Dump VCD
     if dump_vcd:
@@ -32,11 +38,13 @@ class TestHarness (Model):
 
     # Translation
     if test_verilog:
-      s.reducer = TranslationTool( s.reducer )
+      s.reducer = TranslationTool( s.reducer     )
 
     # Connect
-    s.connect( s.src.out,      s.reducer.req )
-    s.connect( s.reducer.resp, s.sink.in_    )
+    s.connect( s.src.out,           s.reducer.req     )
+    s.connect( s.reducer.resp,      s.sink.in_        )
+    s.connect( s.reducer.mem_req,    s.mem.reqs[0]     )
+    s.connect( s.reducer.mem_resp,   s.mem.resps[0]    )
 
   def done(s):
     return s.src.done and s.sink.done
@@ -50,19 +58,22 @@ class TestHarness (Model):
 # mk_req_msg
 #-------------------------------------------------------------------------
 
-def mk_req_msg( data, type ):
+def mk_req_msg( data, digit, type_ ):
   msg       = ReducerReqMsg()
   msg.data  = data
-  msg.type_ = type
+  msg.digit = digit
+  msg.type_ = type_
   return msg
 
 #-------------------------------------------------------------------------
 # mk_resp_msg
 #-------------------------------------------------------------------------
 
-def mk_resp_msg( data ):
+def mk_resp_msg( data, digit, type_ ):
   msg       = ReducerRespMsg()
   msg.data  = data
+  msg.digit = digit
+  msg.type_ = type_
   return msg
 
 #-------------------------------------------------------------------------
@@ -70,8 +81,7 @@ def mk_resp_msg( data ):
 #-------------------------------------------------------------------------
 
 basic_msgs = [
-  mk_req_msg( 1, 1 ), mk_resp_msg( 1 ), 
-  mk_req_msg( 0, 1 ), mk_resp_msg( 0 ),
+  mk_req_msg( 0, 0, 1 ), mk_resp_msg( 0, 0, 1 ), 
 ]
 
 #-------------------------------------------------------------------------
@@ -79,17 +89,30 @@ basic_msgs = [
 #-------------------------------------------------------------------------
 
 test_case_table = mk_test_case_table([
-  (               "msgs       src_delay  sink_delay" ),
-  [ "basic_0x0",  basic_msgs, 0,         0           ], 
+  (               "msgs       stall     latency    src_delay  sink_delay" ),
+  [ "basic_0x0",  basic_msgs, 0,        0,         0,         0,          ], 
 ])
 
+
+
 #-------------------------------------------------------------------------
-# Test cases
+# Run Test
 #-------------------------------------------------------------------------
+
+def run_test( reducer, test_params, dump_vcd, test_verilog=False ):
+
+  reducer_reqs   = test_params.msgs[::2]
+  reducer_resps  = test_params.msgs[1::2]
+
+  th = TestHarness( reducer, reducer_reqs, reducer_resps,
+                    test_params.stall, test_params.latency,
+                    test_params.src_delay, test_params.sink_delay,
+                    dump_vcd, test_verilog )
+
+  run_sim( th, dump_vcd, max_cycles=50 )
+
 
 @pytest.mark.parametrize( **test_case_table )
 def test( test_params, dump_vcd ):
-  run_sim( TestHarness( ReducerPRTL,
-                        test_params.msgs[::2], test_params.msgs[1::2],
-                        test_params.src_delay, test_params.sink_delay ),
-           dump_vcd )
+  run_test( ReducerPRTL(), test_params, dump_vcd )
+
