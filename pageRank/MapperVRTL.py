83f04be60e6a5246c990549db87cb21a07e424f8
mq58 <mq58@cornell.edu>
2016-02-24 10:30:48 -0500

    added registers in mapper, random tested

--- pageRank/MapperVRTL.py
+++ pageRank/MapperVRTL.py
@@ -13,22 +13,25 @@ class MapperVRTL( VerilogModel ):
 
     # Interface
 
-    s.in0   = InPort  ( Bits(nbits)   )#[ InPort  ( nbits ) for _ in range( n ) ]
-    s.in1   = InPort  ( Bits(nbits)   )#[ InPort  ( nbits ) for _ in range( n ) ]
-    s.in2   = InPort  ( Bits(nbits)   )#[ InPort  ( nbits ) for _ in range( n ) ]
-    s.in3   = InPort  ( Bits(nbits)   )#[ InPort  ( nbits ) for _ in range( n ) ]
+    s.r_0   = InPort  ( Bits(nbits)   )#[ InPort  ( nbits ) for _ in range( n ) ]
+    s.g_0   = InPort  ( Bits(nbits)   )#[ InPort  ( nbits ) for _ in range( n ) ]
+    s.r_1   = InPort  ( Bits(nbits)   )#[ InPort  ( nbits ) for _ in range( n ) ]
+    s.g_1   = InPort  ( Bits(nbits)   )#[ InPort  ( nbits ) for _ in range( n ) ]
     s.out   = OutPort ( Bits(nbits)   )
 
     # Verilog ports
 
     s.set_ports({
-      'r_0'         : s.in0,
-      'g_0'         : s.in1,
-      'r_1'         : s.in2,
-      'g_1'         : s.in3,
+      'clk'         : s.clk,
+      'reset'       : s.reset,
+
+      'r_0'         : s.r_0,
+      'g_0'         : s.g_0,
+      'r_1'         : s.r_1,
+      'g_1'         : s.g_1,
       'out'         : s.out
     })
 
 
   def line_trace( s ):
-    return "in0 {} in1 {} in2 {} in3 {} => out {}".format( s.in0, s.in1, s.in2, s.in3, s.out )
+    return "r_0 {} * g_0 {} + r_1 {} * g_1 {} => out {}".format( s.r_0, s.g_0, s.r_1, s.g_1, s.out )
