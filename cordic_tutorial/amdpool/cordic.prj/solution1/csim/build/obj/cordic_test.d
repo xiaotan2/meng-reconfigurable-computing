d0602ea207df893ac15c756b4ed471b67579538d
mq58 <mq58@cornell.edu>
2016-05-08 19:51:09 -0400

    remove cordic tutroial

--- cordic_tutorial/amdpool/cordic.prj/solution1/csim/build/obj/cordic_test.d
+++
@@ -1,8 +0,0 @@
-obj/cordic_test.o: ../../../../cordic_test.cpp \
- /research/brg/install/bare-pkgs/x86_64-centos6/pkgs/xilinx-vivado-2015.2/Vivado_HLS/2015.2/include/hls_stream.h \
- ../../../../cordic.h ../../../../typedefs.h \
- /research/brg/install/bare-pkgs/x86_64-centos6/pkgs/xilinx-vivado-2015.2/Vivado_HLS/2015.2/include/ap_int.h \
- /research/brg/install/bare-pkgs/x86_64-centos6/pkgs/xilinx-vivado-2015.2/Vivado_HLS/2015.2/include/etc/ap_int_sim.h \
- /research/brg/install/bare-pkgs/x86_64-centos6/pkgs/xilinx-vivado-2015.2/Vivado_HLS/2015.2/include/etc/ap_private.h \
- /research/brg/install/bare-pkgs/x86_64-centos6/pkgs/xilinx-vivado-2015.2/Vivado_HLS/2015.2/include/etc/ap_fixed_sim.h \
- /research/brg/install/bare-pkgs/x86_64-centos6/pkgs/xilinx-vivado-2015.2/Vivado_HLS/2015.2/include/etc/ap_int_sim.h
--- cordic_tutorial/zedboard/host.h
+++
@@ -1,9 +0,0 @@
-#ifndef HOST_H
-#define HOST_H
-
-#include "typedefs.h"
-
-const int NUM_INPUT = 5;
-const int NUM_OUTPUT = 25;
-
-#endif
